c7359957173629a96cfa00468949fd0052171f9a
jamil001 <47100044+jamil001@users.noreply.github.com>
2019-03-23 22:40:54 +0600

    Update functions.php

--- functions.php
+++ functions.php
@@ -4,21 +4,8 @@ function massively_theme_setup(){
 	load_theme_textdomain( "massively");
 	add_theme_support( "post-thumbnails" );
 	add_theme_support( "title-tag" );
-/*	$motion_custom_header = array(
-		'header-text'        => true,
-		'default-text-color' => '#FFFFFF',
-		'width'              => 1200,
-		'height'             => 600,
-		'flex-height'        => true,
-		'flex-width'         => true,
-
-	);
-	add_theme_support( "custom-header", $motion_custom_header );   */
-
 
 	register_nav_menu( "topmenu", __("Top Menu","massively"));
-
-
 	
 	add_image_size( "massively_post_thumb", 508, 310 );
 	
@@ -41,51 +28,6 @@ function massively_assets(){
 	wp_enqueue_script( 'main-js', get_theme_file_uri( '/assets/js/main.js' ), null, array("jquery"), true );
 }
 add_action( "wp_enqueue_scripts", "massively_assets" );
-/*
-function motion_widgets(){
-	
-	register_sidebar(array(
-		'name'          => __( 'Footer Top Section ', 'massively' ),
-		'id'            => 'footer-top',
-		'description'   => __( 'Footer Top Section Area ', 'massively' ),
-		'class'         => '',
-		'before_widget' => '',
-		'after_widget'  => '',
-		'before_title'  => '<h2>',
-		'after_title'   => '</h2>',
-	));
-	
-
-	
-}
-add_action( "widgets_init", "motion_widgets" );
-
-
-function mamurjor_header(){
-	if (is_front_page()) {
-		if (current_theme_supports("custom-header")) {
-			?>
-				
-				<style>
-					
-					#banner{
-						background-image: url(<?php echo header_image(); ?>);
-						background-size: cover;
-					}
-
-
-				</style>
-
-
-			<?php
-		} 
-		
-	} 
-	
-}
-add_action( "wp_head", "mamurjor_header");
-
-*/
 
 
 
